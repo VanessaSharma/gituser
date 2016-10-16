929932c00dc5a45e853abd0ec110f2cbdc62f259
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-10-16 14:24:03 -0700

    Create additional prototype to show gh user repos.

--- js/gituser.js
+++ js/gituser.js
@@ -3,11 +3,21 @@ var apiKey = require('./../.env').apiKey;
 exports.Repo = function() {
 };
 
-exports.Repo.prototype.getUser = function(gitname) {
+exports.Repo.prototype.getRepo = function(gitname) {
   $.get('http://api.github.com/users/' + gitname + '?access_token=' + apiKey).then(function(response) {
-    displayFunction(gitname, response.main.gitname);
+    displayFunction(gitname, response.main.login);
   }).fail(function(error){
-    $('#showUser').text(error.responseJSON.message);
+    $('.showRepo').text(error.responseJSON.message);
+  });
+};
+
+
+exports.Repo.prototype.getAllRepo = function(gitname){
+  $.get('https://api.github.com/users/'+ gitname + '/repos').then(function(repos_url){
+    for(var i = 0; i<repos_url.length; i++){
+      console.log(repo_url[1]);
+  }).fail(function(error){
+    $('.showAllRepo').text(error.responseJSON.message);
   });
 };
 
