844433b4538f55a6fe866cb548e95d61375be94c
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-10-16 18:12:38 -0700

    Change console.log in order to show repos.

--- build/js/app.js
+++ build/js/app.js
@@ -11,7 +11,7 @@ exports.Repo = function() {
 
 exports.Repo.prototype.getRepo = function(gitname) {
   $.get('https://api.github.com/users/'+ gitname +'?access_token=' + apiKey).then(function(response) {
-    console.log(gitname, response.login);
+    $('.showUserName').text(response.login);
   }).fail(function(error){
     $('.showAllRepo').text(error.responseJSON.message);
   });
@@ -21,7 +21,7 @@ exports.Repo.prototype.getRepo = function(gitname) {
 exports.Repo.prototype.getAllRepo = function(gitname) {
   $.get('https://api.github.com/users/'+ gitname + '/repos').then(function(repos_url) {
     for(var i = 0; i<repos_url.length; i++) {
-      $('#showAllRepo').append('<li>' + repos_url[i].name + '</li>');
+      $('.showAllRepo').append('<li>' + repos_url[i].name + '</li>');
     }
     }).fail(function(error){
       $('.showAllRepo').text(error.responseJSON.message);
